d18f517ed5e26b81f48c6af16c2b5c34d9e30305
Zijian Xu <xuzijian@g.ecc.u-tokyo.ac.jp>
2019-04-27 03:01:53 +0900

    Add sparse_table, union_find

---
+++ data_structure/readme.md
@@ -0,0 +1,11 @@
+# union_find
+実質O(1)でunite, findができる。
+## usage
+unite, same, find, size関数がある。さらに、uf.cntで連結成分の個数がわかる。
+## verified
+https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/1/DSL_1_A
+
+# sparse_table
+RMQを構築O(NlogN), クエリO(1)で返す。
+## verified
+https://www.spoj.com/submit/RMQSQ/
---
+++ data_structure/union_find.cpp
@@ -0,0 +1,37 @@
+struct UnionFind {
+    int n, cnt;
+    vector<int> par, rank, sz;
+
+    UnionFind(int n) : n(n), cnt(n), par(n), rank(n), sz(n, 1) {
+        iota(par.begin(), par.end(), 0);
+    }
+
+    int find(int x) {
+        if (x == par[x]) return x;
+        return par[x] = find(par[x]);
+    }
+
+    bool same(int x, int y) {
+        return find(x) == find(y);
+    }
+
+    int size(int x) {
+        return sz[find(x)];
+    }
+
+    void unite(int x, int y) {
+        x = find(x), y = find(y);
+        if (x == y) return;
+        if (rank[x] < rank[y]) {
+            par[x] = y;
+            sz[y] += sz[x];
+        } else {
+            par[y] = x;
+            sz[x] += sz[y];
+            if (rank[x] == rank[y]) {
+                rank[x]++;
+            }
+        }
+        cnt--;
+    }
+};
